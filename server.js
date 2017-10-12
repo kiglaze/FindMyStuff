472fc0b0ba5a25e78dfd8dd1d24afb80a2c5e417
Kristin Glaze <ikglaze33@gmail.com>
2017-10-11 21:38:47 -0400

    Started to get the sign up form to be able to show up on my actual page. Still have to continue moving code over to adapt the passport example code more to my code. Log in page is not adapted yet.

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+
 // Create an instance of the express app.
 var app = express();
 
 // Specify the port.
 var PORT = process.env.PORT || 3000;
+
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -18,26 +24,39 @@ app.use(express.static("public"));
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: false
 }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({
   type: "application/vnd.api+json"
 }));
 
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// app.get("/", function(req, res) {
+//   res.render("index", {});
+// });
 
-
-app.get("/", function(req, res) {
-  res.render("index", {});
-});
-
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT " + PORT);
+// });
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
