2a7d4350d97b95efd9617e0ebc5e0fdeee3f1bee
Zeynep Dayar <zeynep.dayar@gmail.com>
2017-10-14 11:35:10 -0400

    Added user id to rooms GET api

--- routes/room-api-routes.js
+++ routes/room-api-routes.js
@@ -1,12 +1,19 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    app.get("/api/rooms", function (req, res) {
-        db.Room.findAll().then(function (dbRooms) {
+    // get all rooms that belong to a specific user
+    app.get("/api/rooms/:userId", function (req, res) {
+        db.Room.findAll({
+            where: {
+                UserId: req.params.userId
+            },
+            include: [db.StorageArea]
+        }).then(function (dbRooms) {
             res.json(dbRooms);
         });
     });
 
+    // get a specific room by id
     app.get("/api/rooms/:id", function (req, res) {
         // Here we add an "include" property to our options in our findOne query
         // We set the value to an array of the models we want to include in a left outer join
@@ -21,6 +28,7 @@ module.exports = function (app) {
         });
     });
 
+    // create a new room
     app.post("/api/rooms", function (req, res) {
         // Create (add) a new room
         db.Room.create(req.body).then(function (dbRoom) {
