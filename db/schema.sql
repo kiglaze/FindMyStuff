319751edc46db5c4460a663e1989718416b1d286
Zeynep Dayar <zeynep.dayar@gmail.com>
2017-10-15 17:56:31 -0400

    Updated the schema and seeds sql files, in case TAs look at them while grading

--- db/schema.sql
+++ db/schema.sql
@@ -1,65 +1,36 @@
 CREATE DATABASE find_my_stuff_db;
 USE find_my_stuff_db;
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS rooms;
-DROP TABLE IF EXISTS storage_areas;
-DROP TABLE IF EXISTS storage_area_compartments;
-DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Rooms;
+DROP TABLE IF EXISTS Items;
 
-CREATE TABLE users (
+CREATE TABLE Users (
 	id INT NOT NULL AUTO_INCREMENT,
-    username varchar(265) NOT NULL,
-    name varchar(265) NOT NULL,
+    email varchar(265) NOT NULL,
+    name varchar(265),
     passhash varchar(265) NOT NULL,
-	date TIMESTAMP DEFAULT now(),
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE rooms (
+CREATE TABLE Rooms (
 	id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
     name varchar(265) NOT NULL,
     description varchar(265) NULL,
-	date TIMESTAMP DEFAULT now(),
-	FOREIGN KEY (user_id)
-        REFERENCES users(id)
+    UserId INT NOT NULL,
+	FOREIGN KEY (UserId)
+        REFERENCES Users(id)
         ON DELETE CASCADE,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE storage_areas (
+CREATE TABLE Items (
 	id INT NOT NULL AUTO_INCREMENT,
     name varchar(265) NOT NULL,
     description varchar(265) NULL,
-    room_id INT NOT NULL,
-	date TIMESTAMP DEFAULT now(),
-	FOREIGN KEY (room_id)
-        REFERENCES rooms(id)
-        ON DELETE CASCADE,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE storage_area_compartments (
-	id INT NOT NULL AUTO_INCREMENT,
-    label varchar(265) NOT NULL,
-    description varchar(265) NULL,
-    storage_area_id INT NOT NULL,
-	date TIMESTAMP DEFAULT now(),
-	FOREIGN KEY (storage_area_id)
-        REFERENCES storage_areas(id)
-        ON DELETE CASCADE,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE items (
-	id INT NOT NULL AUTO_INCREMENT,
-    name varchar(265) NOT NULL,
-    description varchar(265) NULL,
-    storage_compartment_id INT NOT NULL,
-	date TIMESTAMP DEFAULT now(),
-	FOREIGN KEY (storage_compartment_id)
-        REFERENCES storage_area_compartments(id)
+    RoomId INT NOT NULL,
+	FOREIGN KEY (RoomId)
+        REFERENCES Rooms(id)
         ON DELETE CASCADE,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,10 @@
-INSERT INTO users (username, name, passhash) values ("kiglaze", "KIG", "asdfasdfasdf");
-INSERT INTO users (username, name, passhash) values ("zdayar", "Zeynep", "qwertyqwerty");
-select * from users;
+INSERT INTO users (email, name, passhash) values ("kiglaze@gmail.com", "KIG", "asdfasdfasdf");
+INSERT INTO users (email, name, passhash) values ("zdayar@gmail.com", "Zeynep", "qwertyqwerty");
+SELECT * FROM users;
 
-INSERT INTO rooms (userid, name, description) values (1, "living room", "the main living room area with the wooden floor");
-INSERT INTO rooms (userid, name, description) values (1, "kitchen", "used for cooking");
-INSERT INTO rooms (userid, name, description) values (1, "storage room", "the storage room");
-INSERT INTO rooms (userid, name, description) values (2, "living room", "the main living room area with the tile floor");
+INSERT INTO rooms (UserId, name, description) values (1, "living room", "the main living room area with the wooden floor");
+INSERT INTO rooms (UserId, name, description) values (1, "kitchen", "used for cooking");
+INSERT INTO rooms (UserId, name, description) values (1, "storage room", "the storage room");
+INSERT INTO rooms (UserId, name, description) values (2, "living room", "the main living room area with the tile floor");
 
 SELECT * FROM rooms;
-
-INSERT INTO storage_areas (roomid, name, description) values (1, "storage ottoman", "the black leather storage ottoman");
-INSERT INTO storage_areas (roomid, name, description) values (2, "cabinets over the stove", "cabinets over the stove");
-SELECT * FROM storage_areas;
